9d7eae49ae5ed07b58045ce3e1ee494369c7dfa0
Usama Sardar <149796528+usamasardar07@users.noreply.github.com>
2023-11-19 19:17:53 +0500

    task.py

--- task.py
+++ task.py
@@ -1,41 +1,47 @@
-def costSlab1(matrix):
-    slab1_cost = sum(unit * 10 for unit in matrix[0] if unit <= 100)
-    print(f"Total bill for slab 1: Rs. {slab1_cost}")
+# Creating 2 dimensional Matrix
+matrix = [[55, 65, 75], [120, 150, 170], [210, 230, 240]]
 
-def costSlab2(matrix):
-    slab2_cost = sum(unit * 15 for unit in matrix[1] if 100 < unit <= 200)
-    print(f"Total bill for slab 2: Rs. {slab2_cost}")
 
-def costSlab3(matrix):
-    slab3_cost = sum(unit * 20 for unit in matrix[2] if 200 < unit <= 300)
-    print(f"Total bill for slab 3: Rs. {slab3_cost}")
+def CostSlab1():
+    s1bill = 10
+    mul1 = s1bill * matrix[0][0]
+    mul2 = s1bill * matrix[0][1]
+    mul3 = s1bill * matrix[0][2]
+    print("\nSlab 1 Bill is as follows;")
+    print(str(mul1) + "\t" + str(mul2) + "\t" + str(mul3) + "\n")
 
-def display_menu(matrix, student_id):
-    print(f"Student ID: {student_id}")
-    print("1. Display bill of slab 1 and slab 2")
-    print("2. Display bill of slab 3")
-    print("Press any other key to exit")
 
-    while True:
-        choice = input("Enter your choice: ")
+def CostSlab2():
+    s2bill = 15
+    mul1 = s2bill * matrix[1][0]
+    mul2 = s2bill * matrix[1][1]
+    mul3 = s2bill * matrix[1][2]
+    print("Slab 2 Bill is as follows;")
+    print(str(mul1) + "\t" + str(mul2) + "\t" + str(mul3) + "\n")
 
-        if choice == '1':
-            costSlab1(matrix)
-            costSlab2(matrix)
-        elif choice == '2':
-            costSlab3(matrix)
-        else:
-            print("Exiting the program...")
-            break
 
-# Source data - 2D list
-source_data = [
-    [55, 65, 75],
-    [120, 150, 170],
-    [210, 230, 240]
-]
+def CostSlab3():
+    s3bill = 20
+    mul1 = s3bill * matrix[2][0]
+    mul2 = s3bill * matrix[2][1]
+    mul3 = s3bill * matrix[2][2]
+    print("\nSlab 3 Bill is as follows;")
+    print(str(mul1) + "\t" + str(mul2) + "\t" + str(mul3) + "\n")
 
-student_id = "21MDBCS126"  # Replace this with the actual student ID
 
-# Display menu and process choices
-display_menu(source_data, student_id)
+def display():
+    print("My Student ID is ABC101")
+    print("Press 1 to display Slab 1 & 2 Bills")
+    print("Press 2 to display Slab 3 Bills")
+    print("Press any other key to Exit")
+    choice = input("\nEnter your Choice : ")
+    if choice == '1':
+        CostSlab1()
+        CostSlab2()
+    elif choice == '2':
+        CostSlab3()
+    else:
+        print("Wrong input key, Program terminated!")
+
+
+display()
